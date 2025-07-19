37bc0e611a51660a93b6f41106569fa0d4fede50
Разиль <razilchick@mail.ru>
2025-07-19 13:32:48 +0300

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Эта строчка самая важная
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
